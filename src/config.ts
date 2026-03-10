79b0bb41ba11d70d5b2bd3aa8e7451f92435674a
sylar <xjg@zjca.com.cn>
2026-03-10 15:58:49 +0800

    feat: 博客统计

--- src/config.ts
+++ src/config.ts
@@ -31,4 +31,6 @@ export const SITE = {
   dir: "ltr", // "rtl" | "auto"
   lang: "zh-CN", // html lang code. Set this empty and default will be "en"
   timezone: "Asia/Shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  // 博客创建日期（用于计算运行天数）
+  createdAt: "2025-03-17",
 } as const;
